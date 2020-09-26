860f29f0ede997546a4a0f846b6d6cc810094565
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-09-26 11:40:49 -0700

    Added selection and next logic

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import { useParams } from 'react-router-dom';
-import { Row, Col, Card, Button, Divider, Typography } from 'antd';
+import { Row, Col, Card, Button, Divider, Typography, Form } from 'antd';
 import Question from '../Question'
 const { Title } = Typography;
 
 const Test = () => {
   const { id } = useParams();
   const [questionId, setQuestionId] = useState(1);
+  const [userAnswerId, setUserAnswerId] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [question, setQuestion] = useState();
 
@@ -23,27 +24,43 @@ const Test = () => {
   }, []);
 
   const selectAnswer = e => {
-    console.log(e.target.value);
+    setUserAnswerId(e.target.value);
+  }
+
+  const onSubmit = e => {
+    if(!userAnswerId) {
+      return;
+    }
+
+    setQuestion({...question, userAnswerId });
+    setQuestionId(questionId++);
+    setQuestion({...questions[questionId - 1]});
   }
 
   return (
-    <Card className="main-card">
-      {
-        question ? <Question question={question} selectAnswer={selectAnswer}></Question> : ''
-      }
+    <Form onFinish={onSubmit}>
+      <Card className="main-card">
+        {
+          question ? <Question question={question} selectAnswer={selectAnswer}></Question> : ''
+        }
 
-      <Row justify="center">
-        <Col span={16}>
-            <Button type="primary" size="large" block>Next</Button>
-        </Col>
-      </Row>
-      <Divider orientation="center"></Divider>
-      <Row justify="left">
-        <Col span={16}>
-          <Title level={5}>Number of questions: {questionId}/{questions.length}</Title>
-        </Col>
-      </Row>
-    </Card>
+        <Row justify="center">
+          <Col span={16}>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" size="large" block>
+                {questionId === questions.length ? 'Finish' : 'Next'}
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Divider orientation="center"></Divider>
+        <Row justify="left">
+          <Col span={16}>
+            <Title level={5}>Number of questions: {questionId}/{questions.length}</Title>
+          </Col>
+        </Row>
+      </Card>
+    </Form>
   )
 }
 
