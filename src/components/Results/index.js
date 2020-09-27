5ca2906f2ddc9b09df4b459b0d1f2165f54990ea
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-09-26 21:16:56 -0700

    Updated results page values

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import '../../App.css';
 import './style.css';
 import { Link } from 'react-router-dom';
-import { Card, Typography, Button, Row, Col } from 'antd';
+import { Typography, Button, Row, Col } from 'antd';
 const { Title } = Typography;
 
 const Results = ({ topic, questions }) => {
 
+  const correctAnswers = questions.filter((ques) => ques.answerId === ques.userAnswerId) || [];
+
   return (
     <>
-    <Title level={4}>{topic} Quiz Test Results</Title> 
-      <Title level={5}>Correct Answers: </Title> 
-      <Title level={5}>Total questions: </Title> 
-      <Title level={5}>Percentage:  </Title> 
+    <Title level={4}>{topic.toUpperCase()} Quiz Test Results</Title> 
+      <Title level={5}>Correct Answers: {correctAnswers.length}</Title> 
+      <Title level={5}>Total questions: {questions.length}</Title> 
+      <Title level={5}>Percentage:  {Math.round((correctAnswers.length / questions.length) * 100)} %</Title> 
 
-      <Row>
+      <Row gutter={[16, 16]}>
         <Col span={24}>
           <Link to="/">
             <Button type="primary" size="large" block>Take Another Test</Button>
           </Link>
         </Col>
       </Row>
-      <Row justify="center">
+      <Row justify="center" gutter={[16, 16]}>
         <Col span={24}>
           <Link>
             <Button type="primary" size="large" block>Review Questions</Button>
